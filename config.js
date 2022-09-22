aeb5edf3dca86ed20b0c4fbfe9066adca06d3049
RoyalFoxy <manuel.oesch@hispeed.ch>
2022-09-22 22:17:58 +0200

    v1.0.4

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ const CONFIG = {
   bentoLayout: "bento", // 'bento', 'lists', 'buttons'
 
   // Weather
-  weatherKey: "InsertYourAPIKeyHere123456", // Write here your API Key
+  weatherKey: "8b5020f83e22fcbf91db50492ee2072e", // Write here your API Key
   weatherIcons: "OneDark", // 'Onedark', 'Nord', 'Dark', 'White'
   weatherUnit: "C", // 'F', 'C'
   language: "en", // More languages in https://openweathermap.org/current#multi
